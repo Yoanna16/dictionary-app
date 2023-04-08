2365f320242a81e8b5eee73b325ff31340fa8206
Yoanna16 <yoannaborisova@gmail.com>
2023-04-08 20:06:09 +0200

    add meaning styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/<word>'
     <nav className="my-2 h-14 flex flex-row items-center">
       <h3 className="text-indigo-600 font-bold text-2x1">WordFinder</h3>
     </nav>
-    <input type="text" className="m-5 w-full bg-gray-100 border-none outline-none rounded-lg px-3 py-4 shadow" />
+    <input type="text" className="w-full bg-gray-100 border-none outline-none rounded-lg px-3 py-4 shadow" />
     <button className="-mx-16 bg-gray-300 px-3 py-4 rounded-lg">
       Search
     </button>
@@ -20,9 +20,18 @@ const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/<word>'
     </div>
 
     <div className="my-3">
-      <div>
-        
+      <div className="flex flex-row items-center text-lg font-bold font-serif">
+        <p>Noun</p>
+        <hr className="w-full ml-10"/>
       </div>
+      <p className="text-gray-800 mt-3 font-semibold">Meaning</p>
+      <ul className="list-disc px-10 text-gray-800 text-sm space-y-2">
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi dolore minima voluptate molestiae adipisci. Eos, sint?</li>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi dolore minima voluptate molestiae adipisci. Eos, sint?</li>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi dolore minima voluptate molestiae adipisci. Eos, sint?</li>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi dolore minima voluptate molestiae adipisci. Eos, sint?</li>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi dolore minima voluptate molestiae adipisci. Eos, sint?</li>
+      </ul>
     </div>
     </div>
   )
